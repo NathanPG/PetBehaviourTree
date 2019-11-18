a1b02638169921ec33af7a8d6fdb8597561186d2
Unknown <shans2@rpi.edu>
2019-11-18 00:55:34 -0500

    basic tree, eats food

---
+++ Assets/Scripts/InputListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputListener : MonoBehaviour
+{
+    public Manager dog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dog = GameObject.Find("Boggie").GetComponent<Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string inputstring = Input.inputString;
+
+        // Look for a number key click
+        if (inputstring.Length > 0) {
+            Debug.Log(inputstring);
+
+            if (inputstring[0] == 'f') {
+                dog.dish = 100;
+            }
+
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Panda;
+
+public class Manager : MonoBehaviour {
+
+    private float timer;
+
+    public float hunger;
+    public float stamina;
+    public float sanity;
+    public float contentment;
+    public float bladder;
+
+    public float dish;
+
+
+    [Task]
+    bool EatFood() {
+        Debug.Log("Dish: " + dish + ", hunger: " + hunger);
+        float amount = 100 - hunger;
+        if(dish >= amount) {
+            dish -= amount;
+            hunger += amount;
+            Debug.Log("ate some");
+            return true;
+        } else if(dish < amount && dish > 0) {
+            hunger += dish;
+            dish = 0;
+            Debug.Log("ate all");
+            return true;
+        } else{
+            Debug.Log("no food");
+            return false;
+        }
+    }
+
+    [Task]
+    bool IsHungry() {
+        Debug.Log("Hunger: " + hunger);
+        return hunger > 60;
+    }
+
+    private void Start() {
+        timer = 0;
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        hunger -= Time.deltaTime;
+        
+    }
+   
+   
+}
